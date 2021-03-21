1da6a9b94f74da239e3405f48f80bd69f3d6d860
Zach Soles <zac.soles@gmail.com>
2021-03-20 22:26:48 -0800

    Removal of links
    have to fix displaying mobile bugs

--- src/Components/Homepage/Homepage.js
+++ src/Components/Homepage/Homepage.js
@@ -14,7 +14,7 @@ function Homepage() {
         {/* <img className="img" src={image} alt="An Image of ME"/> */}
         <h1 className = "Homeh1" >Zachery Soles</h1>
         <p className = "Homep" >Aerospace Engineer | Software Engineer</p>
-        <a href="https://github.com/ZachSoles"  target="_blank" rel="noopener noreferrer" style={{padding: "10px"}}>
+        {/* <a href="https://github.com/ZachSoles"  target="_blank" rel="noopener noreferrer" style={{padding: "10px"}}>
           <img width = "40" height = "40" alt="github" src={github}/>
         </a>
         <a href="https://www.instagram.com/zach__soles/" target="_blank" rel="noopener noreferrer" style={{padding: "10px"}}>
@@ -22,7 +22,7 @@ function Homepage() {
         </a>
         <a href="https://www.linkedin.com/in/zachery-soles-9b00a212a/" target="_blank" rel="noopener noreferrer" style={{padding: "10px"}}>
           <img width = "40" height = "40" alt="linkedin" src={linkedin}/>
-        </a>
+        </a> */}
         {/* <Button href = "/#/about" variant = "outline-light" >Click here to see about me</Button> */}
     </div>
   );
